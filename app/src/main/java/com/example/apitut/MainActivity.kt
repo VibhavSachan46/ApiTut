43bc1e0da0b15068a69d59fd1d8625e04ea3d7e4
Vibhav <vibhavsachan743@gmail.com>
2023-03-18 16:07:50 +0530

    Step 1 2 3

---
+++ app/src/main/java/com/example/apitut/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.apitut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java )// gson is used here
+
+
+        val retrofitData = retrofitBuilder.getProductData()
+
+        retrofitData.enqueue(object : Callback<MyData?> {
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                //if api call is success then use the data of api and show in your app
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        }) //enque method ends
+    }
+}
